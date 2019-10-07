8dc68324721be06dd6c54b4773f836ed28ed6489
hakase0724 <35761029+hakase0724@users.noreply.github.com>
2019-10-07 10:43:35 +0900

    ローカル変数で持ってても大丈夫なものを変更

--- TimeBaseLoopExecuter/TimeBaseLoopExecuter/TimeBaseLoopExecuter.cpp
+++ TimeBaseLoopExecuter/TimeBaseLoopExecuter/TimeBaseLoopExecuter.cpp
@@ -5,23 +5,21 @@ double TimeBaseLoopExecuter::TimeAdjustment()
 	// 今の時間を取得
 	QueryPerformanceCounter(&mTimeEnd);
 	// (今の時間 - 前フレームの時間) / 周波数 = 経過時間(秒単位)
-	mFrameTime = static_cast<double>(mTimeEnd.QuadPart - mTimeStart.QuadPart) / static_cast<double>(mTimeFreq.QuadPart);
+	double frameTime = static_cast<double>(mTimeEnd.QuadPart - mTimeStart.QuadPart) / static_cast<double>(mTimeFreq.QuadPart);
 	double fps = 0.0;
 	//処理時間に余裕がある場合はその分待つ
-	if (mFrameTime < FRAME_TIME)
+	if (frameTime < FRAME_TIME)
 	{
 		//Sleepの時間を計算
-		DWORD sleepTime = static_cast<DWORD>((FRAME_TIME - mFrameTime) * 1000);
+		DWORD sleepTime = static_cast<DWORD>((FRAME_TIME - frameTime) * 1000);
 		timeBeginPeriod(1);
 		//寝る
 		Sleep(sleepTime);
 		timeEndPeriod(1);
 	}
-	else 
-	{
-		fps = 1 / mFrameTime;
+	fps = 1 / frameTime;
 
-		//FPSを出力窓に出す
+	//FPSを出力窓に出す
 #ifdef _DEBUG
 #ifdef UNICODE
 		std::wstringstream stream;
@@ -35,7 +33,6 @@ double TimeBaseLoopExecuter::TimeAdjustment()
 			OutputDebugString(stream.str().c_str());
 		}
 #endif // _DEBUG
-	}
 	mTimeStart = mTimeEnd;
 	return fps;
 }
